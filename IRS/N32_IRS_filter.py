c7f42a5f94d0d6a35efef7f5d11805a590642524
shrikant prabhu <shrikant.prabhu@motorolasolutions.com>
2020-07-18 13:27:24 -0700

    initial commit.

---
+++ IRS/N32_IRS_filter.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from pymongo import MongoClient
+
+def insert_mongoDb(data_to_insert):
+  client = MongoClient(
+      "mongodb+srv://democracylab:DemocracyLab2019@shelter-rm3lc.azure.mongodb.net/shelter?retryWrites=true&w=majority")
+
+  print("Successfully inserted " + str(data_to_insert.size) + "rows to mongodb.")
+
+
+def remove_duplicates(filtered_data):
+  client = MongoClient(
+      "mongodb+srv://democracylab:DemocracyLab2019@shelter-rm3lc.azure.mongodb.net/shelter?retryWrites=true&w=majority")
+
+  db =
+  return filtered_data
+
+if __name__ == "__main__":
+  print("Reading the input csv file.")
+  data = pd.read_csv('~/Downloads/PuertoRico.csv', usecols=['EIN', 'NAME', 'STREET', 'CITY', 'STATE', 'ZIP', 'NTEE_CD'])
+  filter_exp = data['NTEE_CD'] == 'B20*'
+  req_data = data[filter_exp]
+
+  if req_data.size > 0:
+    print("found " + str(req_data.size) + " rows matching the NTEE code.")
+    data_to_insert = remove_duplicates(req_data)
+
+    if data_to_insert.size > 0:
+      insert_mongoDb(data_to_insert)
