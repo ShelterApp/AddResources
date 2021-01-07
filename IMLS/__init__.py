7b0ffe55b83689af362941ef2d5164bf724cb689
Shah, Jigar <jshah@digimarc.com>
2021-01-07 09:57:20 -0800

    Removed the check for new data in init.py

--- IMLS/__init__.py
+++ IMLS/__init__.py
@@ -16,13 +16,7 @@ def main(mytimer: func.TimerRequest, context: func.Context) -> None:
     ).isoformat()
     conn_string = os.environ['MONGO_DB_CONNECTION_STRING']
     client = MongoClient(conn_string)['shelter']
-    scraped_update_date = imls_scaper.scrape_updated_date()
-    stored_update_date = imls_scaper.retrieve_last_scraped_date(client)
-
-    if stored_update_date is not None:
-        if scraped_update_date < stored_update_date:
-            logging.info('No new IMLS data. Goodbye...')
-            sys.exit()
+   
     imls_scaper.main_scraper(client)
     if mytimer.past_due:
         logging.info('The timer is past due!')
