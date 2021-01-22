9af4aa7e84505aea09619133717b7b67f18bde01
Shrikant Prabhu <prabhushrikant@gmail.com>
2021-01-21 16:35:14 -0800

    fix the azure function invoke script.

--- invoke_az_scraper_func.ps1
+++ invoke_az_scraper_func.ps1
@@ -1,14 +1,35 @@
-#script to invoke a particular azure function scraper.
-param($scraperName)
+param([Parameter(Mandatory)][string]$scraperName)
+
+# Script to invoke a particular azure function scraper.
 $ErrorActionPreference = "Stop"
 
 function ConsoleWriteline($arg = "") {
     Write-Information $arg -InformationAction Continue
 }
 
-*************************************************
+function CheckPowershellVersion() {    
+    $pwshVersion = (Get-Host).Version
+    if ($pwshVersion.Major -lt 7) {
+        "Current Powershell version"
+        $pwshVersion #display current version.
+        "This script requires powershell version 7 or more. Install latest powershell version from here: https://github.com/PowerShell/PowerShell/releases"
+        exit
+    }
+}
+
+# *************************************************
 # All functions go above this line.
-*************************************************
+# *************************************************
+
+CheckPowershellVersion
+
+if (Get-Module -Name AzureRM -ListAvailable) {
+    Write-Warning -Message ('Az module not installed. Having both the AzureRM and ' +
+      'Az modules installed at the same time is not supported.')
+} else {
+    Install-Module -Name Az -AllowClobber -Scope CurrentUser
+}
+
 if ($null -eq $scraperName -or $scraperName -eq "") {
     "Invalid name of the scraper specified. Invoke script with parameter `-scraperName <name>` . Allowed names are: " /
     + "IRS" /
@@ -22,21 +43,20 @@ $secretName = 'scraper-function-api-key'
 $functionKey = (Get-AzKeyVaultSecret -VaultName $vaultName -Name "$secretName").SecretValue | ConvertFrom-SecureString -AsPlainText
 
 $funcUrl = "https://shelterapp-scrapers.azurewebsites.net/admin/functions/$scraperName"
-
 $headers = @{
-    'Content-Type'  = "application/json"
     'x-functions-key' = "$functionKey"
 }
-$requestBody = "{}" | ConvertTo-Json
-$azureResponse = Invoke-RestMethod -Uri $funcUrl -Method Post -Body $requestBody -Headers $headers -StatusCodeVariable responseStatus -SkipHttpErrorCheck
+$requestBody = "{}"
+$azureResponse = Invoke-RestMethod -Uri $funcUrl -Method 'Post'-Body $requestBody -Headers $headers -ContentType 'application/json' -StatusCodeVariable responseStatus
 
 if ($responseStatus -eq '202') {
     "$scraperName scraper started successfully."
 } 
 else {
     "Couldn't start the scraper. Status code: $responseStatus and Error: "
-    $azureResponseJson = $azureResponse | ConvertTo-Json
-    ConsoleWriteline $azureResponseJson
+    # $azureResponseJson = $azureResponse | ConvertTo-Json
+    # ConsoleWriteline $azureResponseJson
+    ConsoleWriteline $azureResponse
     return 1 #Failed.
 }
 
