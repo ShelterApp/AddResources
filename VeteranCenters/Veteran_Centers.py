df814d72b28395227730f89b706a18c2c64cfa97
GuidoTaboas <guidotaboas@gmail.com>
2021-01-25 18:23:32 +0100

    Changed name of TmpCollection and addded filter to remove resources with CLOSED as status

--- VeteranCenters/Veteran_Centers.py
+++ VeteranCenters/Veteran_Centers.py
@@ -24,7 +24,7 @@ from shared_code.utils import (
 )
 from shared_code.base_scraper import BaseScraper
 
-api_key = os.environ[VETERANS_API_KEY]
+api_key = os.environ['VETERANS_API_KEY']
 
 class VeteranCentersScraper(BaseScraper):
 
@@ -33,7 +33,7 @@ class VeteranCentersScraper(BaseScraper):
         headers = {'apikey': api_key, 'accept': 'text/csv'}
         response = requests.get(url, headers=headers)
         response = StringIO(response.text)
-        df = pd.read_csv(response, usecols=self.extract_usecols)
+        df = pd.read_csv(response, usecols= self.extract_usecols)
         df = df[df['id'].str.startswith('v')]
         df.drop_duplicates(
             subset=self.drop_duplicates_columns,
@@ -42,6 +42,7 @@ class VeteranCentersScraper(BaseScraper):
         )
         df = df.fillna("")
         df = df.astype(str)
+        df.drop(df.loc[df['operating_status_code'] == 'CLOSED'].index, inplace=True)
         df['address1'] = df['physical_address_1'] + ' ' + df['physical_address_2'] + ' ' + df['physical_address_3']
         df['serviceSummary'] = df['facility_type'] + ' ' + df['classification']
         df['phone'] = df['phone_main'].str.replace(r'(\d{3})\s*[-.]\s*(\d{3}\s*[-.]\s*\d{4})', r'(\1) \2')
@@ -49,8 +50,9 @@ class VeteranCentersScraper(BaseScraper):
             lambda z: z[0:5] if "-" in z else z
         )
         df.drop(['phone_main', 'facility_type', 'physical_zip', 'physical_address_3', 'physical_address_2',
-                 'physical_address_1', 'classification', 'id'], axis=1, inplace=True)
+                 'physical_address_1', 'classification', 'id', 'operating_status_code'], axis=1, inplace=True)
         df.rename(columns=self.rename_columns, inplace=True)
+        df.reset_index(inplace=True, drop=True)
         return df
 
 
@@ -64,7 +66,8 @@ veteran_shelters_scraper = VeteranCentersScraper(
     data_format="CSV",
     extract_usecols=[
         "name", "id", "facility_type", "website", "phone_main", "physical_city", "physical_state",
-        "physical_zip", "physical_address_3", "physical_address_2", "physical_address_1", "classification"
+        "physical_zip", "physical_address_3", "physical_address_2", "physical_address_1", "classification",
+        "operating_status_code"
     ],
     drop_duplicates_columns=[
         "name", "website", "phone_main", "physical_address_3", "physical_address_2", "physical_address_1"
@@ -74,8 +77,8 @@ veteran_shelters_scraper = VeteranCentersScraper(
     },
     service_summary="",
     check_collection="services",
-    dump_collection="tmpVeteranCenters",
-    dupe_collection="tmpVeteranCentersDuplicates",
+    dump_collection="tmpVeteransTestData",
+    dupe_collection="tmpVeteransTestDataDuplicates",
     data_source_collection_name=data_source_name,
     collection_dupe_field='name',
 )
