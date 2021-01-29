c516a820410797890b7854ac89445ddd3b2c8bc7
picklesueat <jgutstadt48@gmail.com>
2021-01-29 11:23:55 -0600

    fixes

--- summer_meal_sites_2020/summer_meal_sites_2020_scraper.py
+++ summer_meal_sites_2020/summer_meal_sites_2020_scraper.py
@@ -20,11 +20,12 @@ from shared_code.utils import (
 )
 from shared_code.base_scraper import BaseScraper
 
-class Summer_Meal_Sites_2020_Scraper(BaseScraper):
+class SummerMealSitesScraper(BaseScraper):
+    payload = requests.get('https://opendata.arcgis.com/datasets/9efd2e8ba3104b88921b06fa3f70defb_0.geojson')
     def grab_data(self):
         #Get the data
-        data = requests.get('https://opendata.arcgis.com/datasets/9efd2e8ba3104b88921b06fa3f70defb_0.geojson')
-        data = data.json()
+        data = payload.json()
+
         data = data['features']
         new_data = []
         for item in data:
@@ -33,14 +34,13 @@ class Summer_Meal_Sites_2020_Scraper(BaseScraper):
         df = pd.DataFrame(new_data)
 
         #Extract useful cols
-        df = df.drop([ 'contactFirstName', 'contactLastName', 'contactPhone',
-       'sponsoringOrganization', 'startDate', 'endDate', 'daysofOperation',
-       'comments', 'breakfastTime', 'lunchTime', 'snackTimeAM', 'snackTimePM',
-       'dinnerSupperTime', 'mealTypesServed', 'cycleNumber', 'RecordStatus', 'FNSID', 'Created', 'Season', 'County', 'siteAddress2','ext'],
-        axis = 1)
+        df = df[['siteName', 'siteStatus','siteAddress','siteCity',
+        'siteState','siteZip','sitePhone','Country']]
 
         #Removing schools, and non-homeless related resources
-        filter = df['siteName'].str.contains('(school|middle|elementary|high|academy|Academy|learn|Boy|Girl|Fire|Bus|College|Apartment|Route|Magnet)', flags = re.IGNORECASE)
+        ignore_resources_with_keywords = ['school', 'middle', 'elementary', 'high', 'academy','Academy',
+         'learn', 'Boy', 'Girl', 'Fire', 'Bus', 'College', 'Apartment', 'Route', 'Magnet']
+        filter = df['siteName'].str.contains("|".join(ignore_resources_with_keywords_list), flags = re.IGNORECASE)
         df = df[~filter]
         df = df.reset_index()
         df = df.drop(['OBJECTID','index'], axis = 1)
@@ -54,16 +54,15 @@ class Summer_Meal_Sites_2020_Scraper(BaseScraper):
         return super().grab_data(df = df)
 
     def scrape_updated_date(self):
-        data = requests.get('https://opendata.arcgis.com/datasets/9efd2e8ba3104b88921b06fa3f70defb_0.geojson')
-        data = data.headers
+        data = payload.headers
         data = data['x-amz-meta-contentlastmodified']
-        return datetime.strptime(data, '%Y-%m-%dT%H:%M:%S.%fZ')  #Should I add that it is 'UTC' somehow?
+        return datetime.strptime(data, '%Y-%m-%dT%H:%M:%S.%fZ')
 
-
-scraper = Summer_Meal_Sites_2020_Scraper(
-    source="Summer_Meal_Sites_2020_Scraper",
-    data_url = 'https://opendata.arcgis.com/datasets/9efd2e8ba3104b88921b06fa3f70defb_0.geojson',
-    data_page_url = 'https://opendata.arcgis.com/datasets/9efd2e8ba3104b88921b06fa3f70defb_0.geojson',
+data_url = 'https://opendata.arcgis.com/datasets/9efd2e8ba3104b88921b06fa3f70defb_0.geojson'
+scraper = SummerMealSitesScraper(
+    source="SummerMealSitesScraper",
+    data_url = data_url,
+    data_page_url = data_url,
     data_format = "DF",
     extract_usecols=None,
     drop_duplicates_columns=['siteName', 'siteAddress', 'siteZip', 'siteCity', 'siteState'],
@@ -72,9 +71,9 @@ scraper = Summer_Meal_Sites_2020_Scraper(
     },
     service_summary="Food Bank",
     check_collection="services",
-    dump_collection="tmp_summer_meal_sites_2020_dump",
-    dupe_collection="tmp_summer_meal_sites_2020_dupe",
-    data_source_collection_name="summer_meal_sites_2020",
+    dump_collection="tmp_summer_meal_sites_dump",
+    dupe_collection="tmp_summer_meal_sites_dupe",
+    data_source_collection_name="SummerMealSitesScraper",
     collection_dupe_field='name'
     )
 
